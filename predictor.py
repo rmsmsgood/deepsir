5566adb7628aab3a615a949acd1aa14a65e37bf6
rmsmsgood <rmsmsgood@naver.com>
2021-06-18 14:00:28 +0900

    예측기 수정

--- predictor.py
+++ predictor.py
@@ -1,9 +1,13 @@
 from tensorflow import keras
 import pandas as pd
 
+endtime = 10
+
 DATA = pd.read_csv("training_I.csv")
-Y = DATA.iloc[:,2:4].to_numpy()
-X = DATA.iloc[:,4:].to_numpy()
+Y = DATA.iloc[:40000,2:4].to_numpy()
+X = DATA.iloc[:40000,4:(4+endtime)].to_numpy(); X.shape
+# Y = DATA.iloc[:,2:4].to_numpy()
+# X = DATA.iloc[:,4:].to_numpy()
 
 loaded = keras.models.load_model("best.h5")
 
@@ -15,5 +19,5 @@ sum(sum((loaded.predict(X[:300]) - Y[:300])**2))/600
 # ---
 
 DATA = pd.read_csv("대구광역시_기초지자체별 일일 코로나19 확진자 수_20210309.csv", encoding='utf-8')
-real = DATA.iloc[:100,2].to_numpy().reshape(1,100)
-loaded.predict(real)
+real = DATA.iloc[:endtime,2].to_numpy().reshape(1,endtime)
+loaded.predict(real) # seed number 68
